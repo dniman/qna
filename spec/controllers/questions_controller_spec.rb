0b217d6c95ca1a348bcd350fa26df8902e7c6a6a
dniman <dade.niman@gmail.com>
2019-05-31 16:03:22 +0300

    QuestionsController#new

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -26,4 +26,17 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template(:show)
     end
   end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
 end
