6c8eb1bec1d1c399cfd2b0367bb316c0fb66bc5c
dniman <dade.niman@gmail.com>
2019-06-04 19:14:46 +0300

    AnswersController#create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,19 +1,39 @@
 class AnswersController < ApplicationController
+  before_action :find_question, only: %w[new create]
   before_action :set_answer, only: %w[show edit]
 
   def show
   end
 
   def new
-    @answer = Answer.new
+    #@answer = Answer.new
+    @answer = @question.answers.new
   end
 
   def edit
   end
 
+  def create
+    @answer = @question.answers.new(answer_params)
+    
+    if @answer.save
+      redirect_to answer_path(@answer)
+    else
+      render :new
+    end
+  end
+
   private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
 
   def set_answer
     @answer = Answer.find(params[:id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -38,4 +38,30 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template(:edit)
     end
   end
+
+  describe 'POST #create' do
+    let(:question) { create :question } 
+
+    context 'with valid attributes' do
+      it 'saves a new answer to the database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) } 
+        expect(response).to redirect_to answer_path(assigns(:answer))
+      end
+    end
+    
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
+      end
+
+      it 're-render new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
