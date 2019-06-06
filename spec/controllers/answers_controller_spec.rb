4318824a9cd5665d9f5f9413fba31e3c6f67aba5
dniman <dade.niman@gmail.com>
2019-06-06 10:03:29 +0300

    note to the task

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe AnswersController, type: :controller do
   end
 
   describe 'GET #new' do
-    before { get :new, params: { :question_id => answer.question_id } }
+    before { get :new, params: { :question_id => answer.question } }
 
     it 'assigns a new answer to @answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
@@ -44,7 +44,7 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with valid attributes' do
       it 'saves a new answer to the database' do
-        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
       end
 
       it 'redirects to show view' do
@@ -55,7 +55,7 @@ RSpec.describe AnswersController, type: :controller do
     
     context 'with invalid attributes' do
       it 'does not save the answer' do
-        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(question.answers, :count)
       end
 
       it 're-render new view' do
